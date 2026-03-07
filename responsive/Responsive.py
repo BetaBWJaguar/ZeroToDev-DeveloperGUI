7450dea0782f765ff0875b9c1aae4cddf8a74192
BetaBWJaguarr <tunarasimocak@gmail.com>
2026-03-07 03:06:13 +0300

    Update

--- responsive/Responsive.py
+++ responsive/Responsive.py
@@ -2,18 +2,23 @@
 import tkinter as tk
 import ctypes
 
+
+def _set_dpi_awareness():
+    try:
+        ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    except Exception:
+        try:
+            ctypes.windll.user32.SetProcessDPIAware()
+        except Exception:
+            pass
+
+
 class Responsive:
+
     def __init__(self, root: tk.Tk):
         self.root = root
 
-        try:
-            ctypes.windll.shcore.SetProcessDpiAwareness(1)
-        except Exception:
-            try:
-                ctypes.windll.user32.SetProcessDPIAware()
-            except Exception:
-                pass
-
+        _set_dpi_awareness()
         self.screen_width = root.winfo_screenwidth()
         self.screen_height = root.winfo_screenheight()
 
@@ -24,21 +29,24 @@ class Responsive:
             self.window_factor = self.screen_height / 1440
             self.font_factor = max(0.85, (1.0 + self.window_factor) / 2)
 
-    def apply(self, base_width: int, base_height: int, resizable: bool = True) -> tuple:
+    def apply(self, base_width: int, base_height: int, resizable: bool = True):
+
         width = int(base_width * self.window_factor)
         height = int(base_height * self.window_factor)
 
+        width = max(width, 500)
+        height = max(height, 400)
         width = min(width, self.screen_width - 20)
         height = min(height, self.screen_height - 80)
 
         self.root.resizable(resizable, resizable)
 
-        x = (self.screen_width - width) // 2
-        y = (self.screen_height - height) // 2
+        x = max((self.screen_width - width) // 2, 0)
+        y = max((self.screen_height - height) // 2, 0)
+
         self.root.geometry(f"{width}x{height}+{x}+{y}")
 
-
-        return width, height
+        return width, height, self.font_factor
 
 def make_responsive(root: tk.Tk, base_width: int, base_height: int, resizable: bool = True) -> tuple:
     resp = Responsive(root)
